b273b454fa0416dd10613bc7891a688571d1c8b6
Vadyxa715 <vadim_kurdin@mail.ru>
2023-06-06 20:59:40 +0300

    Initial commit

---
+++ src/test/java/com/kurdev/springshop/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurdev.springshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
